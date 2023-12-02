2d9e2768368be8369f9a5102b68bb3753bd13c0c
Michael Pradetto <94888847+Pradetto@users.noreply.github.com>
2023-12-01 23:16:35 -0600

    update actor name to pradetto

--- dist/index.js
+++ dist/index.js
@@ -206,7 +206,7 @@ const waitForDeploymentToStart = async ({
   repo,
   sha,
   environment,
-  actorName = 'github actions',
+  actorName = 'pradetto',
   maxTimeout = 20,
   checkIntervalInMilliseconds = 2000,
 }) => {
@@ -325,7 +325,7 @@ const run = async () => {
       repo,
       sha: sha,
       environment: ENVIRONMENT,
-      actorName: 'github actions',
+      actorName: 'pradetto',
       maxTimeout: MAX_TIMEOUT / 2,
       checkIntervalInMilliseconds: CHECK_INTERVAL_IN_MS,
     });
